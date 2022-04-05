5a9454c39cd5add18ff08478ecd976b9ec626399
shivang <113shivang@gmail.com>
2022-04-06 02:11:11 +0530

    .

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
+const port = process.env.PORT || 8000
 const UserRouter = require('../routes/UserRoute')
 const socketio = require('socket.io')
 const http = require('http')
